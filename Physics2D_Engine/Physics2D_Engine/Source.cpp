c0368139775ccf835b12854c711c5140aa3c3498
mchlkilmer <mchlkilmer@hotmail.com>
2016-11-29 16:39:05 -0800

    Updated with mesh class

--- Physics2D_Engine/Physics2D_Engine/Source.cpp
+++ Physics2D_Engine/Physics2D_Engine/Source.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "mesh.h"
 
 int main(int argc, char* args[]) {
 	std::cout << "helloworld!" << std::endl;
 
 	Display display(800, 600, "HelloWorld");
 
+	Vertex vertices[] = { Vertex(glm::vec3(-0.5,-0.5,0)),
+						  Vertex(glm::vec3(0,0.5,0)),
+						  Vertex(glm::vec3(0.5,-0.5,0))};
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
 	while (!display.getIsClosed()) {
-		display.fill(1.0f, 1.0f, 1.0f,1);
+		display.fill(.1f, .1f, .1f,1);
+
+		mesh.Draw();
+
 		display.Update();
 	}
 	return 0;
