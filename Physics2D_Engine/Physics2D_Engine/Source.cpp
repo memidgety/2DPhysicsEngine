0bc8d13765d78d23c5b3d0b36df804ec3b3bab8b
mchlkilmer <mchlkilmer@hotmail.com>
2016-11-29 21:23:37 -0800

    Updated with Shader Class

--- Physics2D_Engine/Physics2D_Engine/Source.cpp
+++ Physics2D_Engine/Physics2D_Engine/Source.cpp
@@ -2,11 +2,14 @@
 #include <GL/glew.h>
 #include "display.h"
 #include "mesh.h"
+#include "shader.h"
 
 int main(int argc, char* args[]) {
 	std::cout << "helloworld!" << std::endl;
 
 	Display display(800, 600, "HelloWorld");
+
+	Shader shader("./res/basicShader");
 
 	Vertex vertices[] = { Vertex(glm::vec3(-0.5,-0.5,0)),
 						  Vertex(glm::vec3(0,0.5,0)),
@@ -17,6 +20,7 @@ int main(int argc, char* args[]) {
 	while (!display.getIsClosed()) {
 		display.fill(.1f, .1f, .1f,1);
 
+		shader.Bind();
 		mesh.Draw();
 
 		display.Update();
---
+++ Physics2D_Engine/Physics2D_Engine/shader.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <GL/glew.h>
+
+class Shader{
+public:
+	Shader(const std::string& fileName);
+
+	void Bind();
+
+	virtual ~Shader();
+protected:
+private:
+	static const unsigned int NUM_SHADERS = 2;
+	Shader(const Shader& other) {}
+	void operator=(const Shader& other);
+
+	GLuint m_program;
+	GLuint m_shaders[NUM_SHADERS];
+
+};
