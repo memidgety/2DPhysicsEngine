ec831284ab9e649f56cb23f5650125b299c009d8
mchlkilmer <mchlkilmer@hotmail.com>
2016-12-07 02:56:27 -0800

    Updated With Falling Objects

--- Physics2D_Engine/Physics2D_Engine/Source.cpp
+++ Physics2D_Engine/Physics2D_Engine/Source.cpp
@@ -6,34 +6,50 @@
 #include "shader.h"
 #include "body.h"
 
+Position pos1(100, 100);
+
+Position pos2(-60, -30);
+Position pos3(-60, -60);
+Position pos4(-30, -60);
+Position pos5(-30, -30);
+
+Position pos6(-240, 100);
+Position pos7(-260, 0);
+Position pos8(-220, 0);
+
+
 int main(int argc, char* args[]) {
 	std::cout << "helloworld!" << std::endl;
 	
 	int screenWidth = 800;
 	int screenHieght = 800;
 
-	//Circle c1;
-	AABB sq1;
-	Triangle T1;
-	Circle c1;
+	Circle c1(pos1);
+	AABB sq1(pos2, pos3, pos4, pos5);
+	Triangle T1(pos6, pos7, pos8);
 
-	Position pos1(300, 300);
-	Position pos2(800, -600);
-	Position pos3(-800, -600);
+
 
 	Display display(screenWidth, screenHieght, "HelloWorld");
 
 	Shader shader("./res/basicShader");
 
-	while (!display.getIsClosed()) {
-		display.fill(.1f, .1f, .1f,1);
-		
-		c1.DrawCircle(pos1, .2, 36, display);
-		//sq1.draw();
-		//T1.draw(pos1, pos2, pos3, display);
+	while (!display.getIsClosed()) {	
+		display.fill(.1f, .1f, .1f, 1);
+
+		c1.draw(20, 36, display);
+		c1.gravityScale();
+
+		sq1.draw(display);
+	    sq1.gravityScale();
+
+		T1.draw(display);
+		T1.gravityScale();
 		shader.Bind();
 
 		display.Update();
+
+		//display.setIsClosed(true);
 	}
 	return 0;
 }
